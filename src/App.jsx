6273da49188eb9df32665878f1e93549cacd22e9
조민재 <whalswo1993@gmail.com>
2026-09-04 12:13:30 +0900

    first project version

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+export default function App() {
+  return (
+    <div style={{
+      padding: "40px",
+      fontFamily: "Arial"
+    }}>
+      <h1>프로젝트 일정 관리 앱</h1>
+
+      <p>React 앱이 정상적으로 실행되고 있습니다.</p>
+
+      <button>
+        프로젝트 추가
+      </button>
+    </div>
+  );
+}
